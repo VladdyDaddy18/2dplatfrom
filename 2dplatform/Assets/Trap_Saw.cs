7b94b53db3b2b77b33d630affd863c9c57d7bcfd
VladdyDaddy18 <Willis4689@gmail.com>
2022-11-20 22:38:53 -0800

    prefab saw trap

--- 2dplatform/Assets/Trap_Saw.cs
+++ 2dplatform/Assets/Trap_Saw.cs
@@ -6,9 +6,14 @@ public class Trap_Saw : Trap
 {
 
     private Animator anim;
-    [SerializeField] private bool isWorking;
+    private bool isWorking;
     [SerializeField] private Transform[] movePoint;
     [SerializeField] private float speed;
+    [SerializeField] private float coolDown;
+
+    private float coolDownTimer;
+
+    private int movePointIndex;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +23,31 @@ public class Trap_Saw : Trap
     // Update is called once per frame
     void Update()
     {
+        coolDownTimer -= Time.deltaTime;
+
+        bool isWorking = coolDownTimer < 0;
+
         anim.SetBool("isWorking", isWorking);
 
-        transform.position = Vector3.MoveTowards(transform.position, movePoint[0].position, speed * Time.deltaTime);
+        if(isWorking)
+        transform.position = Vector3.MoveTowards(transform.position, movePoint[movePointIndex].position, speed * Time.deltaTime);
 
+        if(Vector2.Distance(transform.position,movePoint[movePointIndex].position) < 0.15f)
+       {    
+              Flip();
+              coolDownTimer = coolDown;
+              movePointIndex ++;
+
+        if(movePointIndex >= movePoint.Length)
+               movePointIndex = 0; 
+        
+        
+        }
+
+    }
+
+    private void Flip()
+    {
+        transform.localScale = new Vector3(1, transform.localScale.y * -1);
     }
 }
