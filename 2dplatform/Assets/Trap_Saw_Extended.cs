b4eda3a9d8320cc4c8b6c8cb736ec77fbd096431
VladdyDaddy18 <Willis4689@gmail.com>
2022-11-21 23:36:38 -0800

    extended trap

--- 2dplatform/Assets/Trap_Saw_Extended.cs
+++ 2dplatform/Assets/Trap_Saw_Extended.cs
@@ -2,55 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trap_Saw : Trap
+public class Trap_Saw_Extended : Trap
 {
-
-    private Animator anim;
-    private bool isWorking;
+   private Animator anim;
+  
     [SerializeField] private Transform[] movePoint;
     [SerializeField] private float speed;
-    [SerializeField] private float coolDown;
-    
 
-    private float coolDownTimer;
 
+    private bool goingForward = true;
     private int movePointIndex;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
-        transform.position = movePoint[0].position;
-
+         anim.SetBool("isWorking", true);
+         Flip();
+         transform.position = movePoint[0].position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        coolDownTimer -= Time.deltaTime;
 
-        bool isWorking = coolDownTimer < 0;
 
-        anim.SetBool("isWorking", isWorking);
-
-        if(isWorking)
         transform.position = Vector3.MoveTowards(transform.position, movePoint[movePointIndex].position, speed * Time.deltaTime);
 
         if(Vector2.Distance(transform.position,movePoint[movePointIndex].position) < 0.15f)
        {    
-              Flip();
-              coolDownTimer = coolDown;
+
+        if(movePointIndex == 0)
+          {goingForward = true;
+            Flip();
+          }
+        if(goingForward)
               movePointIndex ++;
+        else
+            movePointIndex --;  
+        
 
         if(movePointIndex >= movePoint.Length)
-               movePointIndex = 0; 
-        
+              {
+               movePointIndex = movePoint.Length -1; 
+               goingForward = false; 
+               Flip();
+              }
         
         }
 
     }
-    private void Flip()
+     private void Flip()
     {
         transform.localScale = new Vector3(1, transform.localScale.y * -1);
     }
-   
 }
