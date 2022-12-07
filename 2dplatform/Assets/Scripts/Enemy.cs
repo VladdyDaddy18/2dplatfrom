3bb355b86d826a9471a7a62156cf1126bc4144b9
VladdyDaddy18 <Willis4689@gmail.com>
2022-12-06 22:28:52 -1000

    Enemy Mushroom script

--- 2dplatform/Assets/Scripts/Enemy.cs
+++ 2dplatform/Assets/Scripts/Enemy.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] protected LayerMask whatIsGround;
+    [SerializeField] protected float groundCheckDistance;
+    [SerializeField] protected float wallCheckDistance;
+    [SerializeField] protected Transform wallCheck;
+    [SerializeField] protected Transform groundCheck;
+
+    protected bool wallDetected;
+    protected bool groundDetected;
+
+
+
+    protected Animator anim;
+    protected Rigidbody2D rb;
+
+    protected int facingDirection = 1;
+
+    protected virtual void Start() 
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();    
+    }
    
     public void Damage()
     {
@@ -19,5 +40,23 @@ public class Enemy : MonoBehaviour
 
             player.KnockBack(transform);
         }
+    }
+
+    protected virtual void Flip()
+    {
+        facingDirection = facingDirection * -1;
+        transform.Rotate(0,180,0);
+    }
+
+    protected virtual void CollisionCheck()
+    {
+        groundDetected = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+        wallDetected = Physics2D.Raycast(wallCheck.position, Vector2.right * facingDirection, groundCheckDistance, whatIsGround);
+    }
+
+    protected void OnDrawGizmos() 
+    {
+        Gizmos.DrawLine(groundCheck.position, new Vector2(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(wallCheck.position, new Vector2(wallCheck.position.x + wallCheckDistance * facingDirection, wallCheck.position.y));
     }
 }
---
+++ 2dplatform/Assets/Scripts/Enemy_Mushroom.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Mushroom : Enemy
+{
+   [SerializeField] private float speed;
+   [SerializeField] private float idleTime = 2;
+   private float idleTimeCounter;
+
+    protected override void Start()
+    {
+        base.Start();
+        facingDirection = -1;
+    }
+
+    private void Update() 
+    {
+        
+       
+        if(idleTimeCounter <= 0)
+            rb.velocity = new Vector2(speed * facingDirection, rb.velocity.y);   
+
+        idleTimeCounter -= Time.deltaTime; 
+
+        CollisionCheck();
+
+        if(wallDetected || !groundDetected)
+        {
+            idleTimeCounter = idleTime;
+            Flip();
+            
+
+        }
+            
+    }
+}
